7f3fe071782e0674d043c9ecc3a5c262e14537d4
Administrator <Administrator@12W8JFC8BE381PI>
2018-10-05 00:07:53 +0800

    更新调用redis实现方式

--- grapRedPacket/src/main/java/com/tk/redpacket/controller/UserRedPacketController.java
+++ grapRedPacket/src/main/java/com/tk/redpacket/controller/UserRedPacketController.java
@@ -1,15 +1,15 @@
 package com.tk.redpacket.controller;
 
-import com.tk.redpacket.service.IUserRedpacketService;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.Map;
+import com.tk.redpacket.service.IUserRedpacketService;
 
 /**
  * Created by Administrator on 2018-9-26.
@@ -22,12 +22,15 @@ public class UserRedPacketController {
 
     @RequestMapping("/grabRedPacket")
     @ResponseBody
-    public Map<String,Object>  grabRedPacket(@RequestParam("redPacketId") int redPacketId,@RequestParam("userId") int userId){
-        int result = userRedpacketService.grabUserRedPacket(redPacketId,userId);
+    public Map<String, Object> grabRedPacket(@RequestParam("redPacketId") int redPacketId, @RequestParam("userId") int userId) {
+    	//int result = userRedpacketService.grabUserRedPacket(redPacketId, userId);
+    	//int result = userRedpacketService.grabUserRedPacketForUpdate(redPacketId, userId);
+    	//int result = userRedpacketService.grabUserRedPacketForVersion(redPacketId, userId);
+    	int result = userRedpacketService.grabUserRedPacketByRedis(redPacketId, userId);
         boolean flag = result > 0;
-        Map<String,Object> retMap = new HashMap<String, Object>();
-        retMap.put("success",flag);
-        retMap.put("message",flag ? "抢红包成功":"抢红包失败");
-        return  retMap;
+        Map<String, Object> retMap = new HashMap<String, Object>();
+        retMap.put("success", flag);
+        retMap.put("message", flag ? "抢红包成功" : "抢红包失败");
+        return retMap;
     }
 }
