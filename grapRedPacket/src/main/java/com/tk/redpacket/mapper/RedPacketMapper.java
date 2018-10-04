60de47596d735ee6d5689836676c0da34d715cdf
Administrator <Administrator@12W8JFC8BE381PI>
2018-10-05 00:04:47 +0800

    更新mapper

--- grapRedPacket/src/main/java/com/tk/redpacket/mapper/RedPacketMapper.java
+++ grapRedPacket/src/main/java/com/tk/redpacket/mapper/RedPacketMapper.java
@@ -1,33 +1,47 @@
 package com.tk.redpacket.mapper;
 
 import com.tk.redpacket.pojo.RedPacket;
+
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
-/** 红包表实体映射 */
+/**
+ * 红包表实体映射
+ */
 @Mapper
 public interface RedPacketMapper {
 
-	/**
-	 * 获取红包信息
-	 * @param id 编号
-	 * @return 返回红包信息
-	 */
-	public RedPacket schRedPacketById(Integer id);
+    /**
+     * 获取红包信息
+     *
+     * @param id 编号
+     * @return 返回红包信息
+     */
+    public RedPacket schRedPacketById(Integer id);
 
 
-	/***
-	 * 扣减红包数
-	 * @param id 红包编号
-	 * @return 更新记录行数
-	 */
-	public int decreaseRedPacket(Integer id);
+    /***
+     * 扣减红包数
+     * @param id 红包编号
+     * @return 更新记录行数
+     */
+    public int decreaseRedPacket(Integer id);
 
-	/**
-	 * 获取红包信息(for update 语句进行加锁)
-	 * @param id 编号
-	 * @return 返回红包信息
-	 */
-	public RedPacket schRedPacketByIdForUpdate(Integer id);
+    /***
+     * 扣减红包数(使用Version -乐观锁实现)
+     * @param id 红包编号
+     * @return 更新记录行数
+     */
+    public int decreaseRedPacketForVersion(@Param("id") Integer id, @Param("version") Integer version);
+
+
+    /**
+     * 获取红包信息(for update 语句进行加锁)
+     *
+     * @param id 编号
+     * @return 返回红包信息
+     */
+    public RedPacket schRedPacketByIdForUpdate(Integer id);
 }
--- grapRedPacket/src/main/java/com/tk/redpacket/mapper/UserRedPacketMapper.java
+++ grapRedPacket/src/main/java/com/tk/redpacket/mapper/UserRedPacketMapper.java
@@ -1,25 +1,30 @@
 package com.tk.redpacket.mapper;
-import  com.tk.redpacket.pojo.UserRedPacket;
+
+import com.tk.redpacket.pojo.UserRedPacket;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
 
-/** 用户抢红包表实体映射 */
+/**
+ * 用户抢红包表实体映射
+ */
 @Mapper
 public interface UserRedPacketMapper {
 
-	/**
-	 * UserRedPacket的主键查询方法
-	 * @param id 主键值
-	 * @return 返回UserRedPacket实体
-	 */
-	public UserRedPacket schUserRedPacketById(Integer id);
-	
-	/**
-	 * UserRedPacket的添加方法
-	 * @param userRedPacket 承载数据的Bean，执行添加后会更新入库后的主键值
-	 * @return 返回影响的行数
-	 */
-	public Integer addUserRedPacket(UserRedPacket userRedPacket);
+    /**
+     * UserRedPacket的主键查询方法
+     *
+     * @param id 主键值
+     * @return 返回UserRedPacket实体
+     */
+    public UserRedPacket schUserRedPacketById(Integer id);
+
+    /**
+     * UserRedPacket的添加方法
+     *
+     * @param userRedPacket 承载数据的Bean，执行添加后会更新入库后的主键值
+     * @return 返回影响的行数
+     */
+    public Integer addUserRedPacket(UserRedPacket userRedPacket);
 }
