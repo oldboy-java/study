4b281fa33eb9966cf8d1ddd8000fe0bed382730d
Administrator <Administrator@12W8JFC8BE381PI>
2018-10-05 00:06:48 +0800

    从配置文件中获取数据库配置和redis配置

--- grapRedPacket/src/main/java/com/tk/redpacket/config/RootConfig.java
+++ grapRedPacket/src/main/java/com/tk/redpacket/config/RootConfig.java
@@ -1,6 +1,11 @@
 package com.tk.redpacket.config;
 
-import org.apache.commons.dbcp.BasicDataSource;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
 import org.apache.commons.dbcp.BasicDataSourceFactory;
 import org.apache.ibatis.annotations.Mapper;
 import org.mybatis.spring.SqlSessionFactoryBean;
@@ -11,14 +16,18 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.transaction.annotation.TransactionManagementConfigurer;
 
-import javax.sql.DataSource;
-import java.util.Properties;
+import redis.clients.jedis.JedisPoolConfig;
 
 /**
  * SpringIoc配置
@@ -26,8 +35,8 @@ import java.util.Properties;
  */
 @Configuration
 //定义Spring扫描的包和扫描的注解
-@ComponentScan(value = "com.tk.readpacket",includeFilters = {@ComponentScan.Filter(type = FilterType.ANNOTATION,
-value = {Service.class})})
+@ComponentScan(value = "com.tk.readpacket", includeFilters = {@ComponentScan.Filter(type = FilterType.ANNOTATION,
+        value = {Service.class})})
 //启用事务管理器
 @EnableTransactionManagement
 public class RootConfig implements TransactionManagementConfigurer {
@@ -38,20 +47,18 @@ public class RootConfig implements TransactionManagementConfigurer {
      * 初始化数据源
      * @return 数据源连接池
      */
-    @Bean(name="dataSource")
-    public DataSource initDataSource(){
-        if(dataSource != null) {
+    @Bean(name = "dataSource")
+    public DataSource initDataSource() {
+        if (dataSource != null) {
             return dataSource;
         }
+        InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream("jdbc.properties");
         Properties properties = new Properties();
-        properties.setProperty("driverClassName","com.mysql.jdbc.Driver");
-        properties.setProperty("url","jdbc:mysql://localhost:3306/red_packet");
-        properties.setProperty("username","root");
-        properties.setProperty("password","123456");
-        properties.setProperty("maxActive","200");
-        properties.setProperty("maxIdle","20");
-        properties.setProperty("maxWait","30000");
-
+        try {
+            properties.load(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         try {
             dataSource = BasicDataSourceFactory.createDataSource(properties);
         } catch (Exception e) {
@@ -64,8 +71,8 @@ public class RootConfig implements TransactionManagementConfigurer {
      * 配置sqlSessionFactoryBean
      * @return sqlSessionFactoryBean
      */
-    @Bean(name="sqlSessionFactoryBean")
-    public SqlSessionFactoryBean initSqlSessionFactoryBean(){
+    @Bean(name = "sqlSessionFactoryBean")
+    public SqlSessionFactoryBean initSqlSessionFactoryBean() {
         SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
         //设置数据源
         sqlSessionFactoryBean.setDataSource(initDataSource());
@@ -80,8 +87,8 @@ public class RootConfig implements TransactionManagementConfigurer {
      * 初始化自动扫描配置器
      * @return Mapper扫描器
      */
-    @Bean(name="mapperScannerConfigurer")
-    public MapperScannerConfigurer initMapperScannerConfigurer(){
+    @Bean(name = "mapperScannerConfigurer")
+    public MapperScannerConfigurer initMapperScannerConfigurer() {
         MapperScannerConfigurer msc = new MapperScannerConfigurer();
         //设置Mapper扫描基路径
         msc.setBasePackage("com.tk.redpacket");
@@ -104,4 +111,51 @@ public class RootConfig implements TransactionManagementConfigurer {
         dataSourceTransactionManager.setDataSource(initDataSource());
         return dataSourceTransactionManager;
     }
+
+    /***
+     * 创建RedisTemplate
+     * @return
+     */
+    @Bean(name = "redisTemplate")
+    public RedisTemplate initRedisTemplate() {
+        //创建JedisPoolConfig
+        JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
+        InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream("redis.properties");
+        Properties properties = new Properties();
+        try {
+            properties.load(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        jedisPoolConfig.setMaxIdle(Integer.parseInt(properties.getProperty("redis.pool.maxIdle")));//最大空闲连接数
+        jedisPoolConfig.setMaxTotal(Integer.parseInt(properties.getProperty("redis.pool.maxTotal")));//最大连接数
+        jedisPoolConfig.setMaxWaitMillis(Integer.parseInt(properties.getProperty("redis.pool.maxWaitMillis")));//最大等待毫秒数
+
+        //创建JedisConnectionFactory
+        JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory();
+        jedisConnectionFactory.setPoolConfig(jedisPoolConfig);
+        jedisConnectionFactory.setHostName(properties.getProperty("redis.host"));//设置主机
+        jedisConnectionFactory.setPort(Integer.parseInt(properties.getProperty("redis.port")));//设置端口
+        //调用后初始化方法
+        jedisConnectionFactory.afterPropertiesSet();
+
+        //定义序列化器
+        RedisSerializer jdkSerializationRedisSerializer = new JdkSerializationRedisSerializer();
+        RedisSerializer stringRedisSerializer = new StringRedisSerializer();
+
+        //创建RedisTemplate
+        RedisTemplate redisTemplate = new RedisTemplate();
+        //设置连接工厂
+        redisTemplate.setConnectionFactory(jedisConnectionFactory);
+        //设置序列化器
+        redisTemplate.setKeySerializer(stringRedisSerializer);
+        redisTemplate.setValueSerializer(stringRedisSerializer);
+        redisTemplate.setDefaultSerializer(stringRedisSerializer);
+        redisTemplate.setHashKeySerializer(stringRedisSerializer);
+        redisTemplate.setHashValueSerializer(stringRedisSerializer);
+        return redisTemplate;
+    }
+    
+    
+
 }
